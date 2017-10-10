9f401ad93e53e4889cb09e59bbd835f458bed7d1
Alessandro Aleotti <71196@studenti.unimore.it>
2017-10-10 11:14:41 +0200

    creato un autoencoder simile a deepDGA. al momento genera dei DGA molto simili alle famiglie esistenti. testandolo Random Forest viene rilevato come DGA quasi il 100 delle volte.

--- examples/lstm_text_generation.py
+++ examples/lstm_text_generation.py
@@ -1,17 +1,17 @@
 '''Example script to generate text from Nietzsche's writings.
+
 At least 20 epochs are required before the generated text
 starts sounding coherent.
+
 It is recommended to run this script on GPU, as recurrent
 networks are quite computationally intensive.
+
 If you try this script on new data, make sure your corpus
 has at least ~100k characters. ~1M is better.
 '''
 
 from __future__ import print_function
-import tensorflow as tf
-import os
 from keras.models import Sequential
-from keras.callbacks import TensorBoard
 from keras.layers import Dense, Activation
 from keras.layers import LSTM
 from keras.optimizers import RMSprop
@@ -20,20 +20,8 @@ import numpy as np
 import random
 import sys
 
-generator_path = '../dataset/xaa'
-lstm_path = "../saved_models/lstm2"
-
-dirtemp = os.path.join(lstm_path, "tensorboard")
-
-config = tf.ConfigProto(
-    device_count={'GPU': 0}
-)
-sess = tf.Session(config=config)
-os.environ['CUDA_VISIBLE_DEVICES'] = ''
-sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
-
-# path = get_file('nietzsche.txt', origin='https://s3.amazonaws.com/text-datasets/nietzsche.txt')
-text = open(generator_path).read().lower()
+path = get_file('nietzsche.txt', origin='https://s3.amazonaws.com/text-datasets/nietzsche.txt')
+text = open(path).read().lower()
 print('corpus length:', len(text))
 
 chars = sorted(list(set(text)))
@@ -42,7 +30,7 @@ char_indices = dict((c, i) for i, c in enumerate(chars))
 indices_char = dict((i, c) for i, c in enumerate(chars))
 
 # cut the text in semi-redundant sequences of maxlen characters
-maxlen = 15
+maxlen = 40
 step = 3
 sentences = []
 next_chars = []
@@ -59,6 +47,7 @@ for i, sentence in enumerate(sentences):
         X[i, t, char_indices[char]] = 1
     y[i, char_indices[next_chars[i]]] = 1
 
+
 # build the model: a single LSTM
 print('Build model...')
 model = Sequential()
@@ -67,7 +56,7 @@ model.add(Dense(len(chars)))
 model.add(Activation('softmax'))
 
 optimizer = RMSprop(lr=0.01)
-model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer=optimizer)
+model.compile(loss='categorical_crossentropy', optimizer=optimizer)
 
 
 def sample(preds, temperature=1.0):
@@ -79,33 +68,16 @@ def sample(preds, temperature=1.0):
     probas = np.random.multinomial(1, preds, 1)
     return np.argmax(probas)
 
-
-callbacks = [
-    TensorBoard(log_dir=dirtemp,
-                write_graph=False,
-                write_images=False,
-                histogram_freq=0),
-]
-
 # train the model, output generated text after each iteration
 for iteration in range(1, 60):
     print()
     print('-' * 50)
     print('Iteration', iteration)
-
     model.fit(X, y,
-              validation_split=0.10,
-              # metrics=['accuracy'],
-              # callbacks=callbacks,
               batch_size=128,
               epochs=1)
 
     start_index = random.randint(0, len(text) - maxlen - 1)
-
-    json_model = model.to_json()
-    dirmod = os.path.join(lstm_path, 'model_architecture.json')
-    open(dirmod, 'w').write(json_model)
-    model.save_weights(os.path.join(lstm_path, 'model_weitghts.h5'), overwrite=True)
 
     for diversity in [0.2, 0.5, 1.0, 1.2]:
         print()
@@ -114,30 +86,21 @@ for iteration in range(1, 60):
         generated = ''
         sentence = text[start_index: start_index + maxlen]
         generated += sentence
-        print('----- Generating with seed: ' + sentence)
+        print('----- Generating with seed: "' + sentence + '"')
         sys.stdout.write(generated)
 
         for i in range(400):
             x = np.zeros((1, maxlen, len(chars)))
-
             for t, char in enumerate(sentence):
                 x[0, t, char_indices[char]] = 1.
 
-            print(x)
             preds = model.predict(x, verbose=0)[0]
-            print(preds)
             next_index = sample(preds, diversity)
             next_char = indices_char[next_index]
 
             generated += next_char
-            print(generated)
             sentence = sentence[1:] + next_char
-            print(sentence)
 
             sys.stdout.write(next_char)
             sys.stdout.flush()
-
         print()
-
-        with open(os.path.join(lstm_path, "generated.txt"), 'a') as f:
-            f.write(generated)
--- examples/try.py
+++ examples/try.py
@@ -1,111 +1,123 @@
 # LSTM and CNN for sequence classification in the IMDB dataset
-import numpy as np
 import string
 
+import numpy as np
 import pandas as pd
-from keras.datasets import imdb
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
-from keras.layers.convolutional import Conv1D
-from keras.layers.convolutional import MaxPooling1D
-from keras.layers.embeddings import Embedding
+from pandas import read_csv
+from keras.layers import Conv1D, MaxPooling1D, Embedding
+from keras.layers import Dense, Input
+from keras.layers import Dropout
+from keras.layers import LSTM, RepeatVector
+from keras.layers.merge import concatenate
+from keras.models import Model
 from keras.preprocessing import sequence
 from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-from keras.utils import to_categorical
-from keras.layers import Dense, Input, Flatten
-from keras.layers import Conv1D, MaxPooling1D, Embedding
-from keras.models import Model
+from keras.utils.vis_utils import plot_model
+from sklearn.preprocessing import LabelBinarizer
 
 # fix random seed for reproducibility
 np.random.seed(7)
-np.set_printoptions(linewidth=200)
+np.set_printoptions(linewidth=2000, threshold=100000)
 # load the dataset but only keep the top n words, zero the rest
 # top_words = 5000
 # (X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=top_words)
-n_samples = 10
+n_samples = 1000
 
-import numpy as np
-import pandas as pd
-from sklearn.preprocessing import LabelBinarizer
-
+# loading db
 lb = LabelBinarizer()
-df = pd.DataFrame(pd.read_csv("../../detect_DGA/datasets/legit_dga_domains.csv", sep=","))
+df = pd.DataFrame(pd.read_csv("../dataset/legitdomains.txt", sep=" ", header=None, names=['domain']))
 if n_samples:
     df = df.sample(n=n_samples, random_state=42)
 X_ = df['domain'].values
-y = np.ravel(lb.fit_transform(df['class'].values))
+# y = np.ravel(lb.fit_transform(df['class'].values))
 
+# preprocessing text
 tk = Tokenizer(char_level=True)
-
-tk.fit_on_texts(string.lowercase + string.digits + '-')
+tk.fit_on_texts(string.lowercase + string.digits + '-'+'.')
 print("word index: %s" % len(tk.word_index))
-# print(tk.word_index)
 seq = tk.texts_to_sequences(X_)
 for x, s in zip(X_, seq):
     print(x, s)
 print("")
-print("X shape before padding: %s" % len(seq))
 X = sequence.pad_sequences(seq)
 print("X shape after padding: " + str(X.shape))
 print(X)
 
-model = Sequential()
-# input_ = Input(shape=(None, X.shape[1]), dtype='int32', name='main_input')
-# x = Embedding(output_dim=20, input_length=X.shape[1], input_dim=37)(input_)
-# conv1 = Conv1D(20, 2, padding='valid', activation='relu', strides='1')(x)
-# # conv2 = Conv1D(10, 3, padding='valid', activation='relu', strides='1')(x)
-# # max1 = MaxPooling1D()(conv1)
-# max2 = MaxPooling1D()(conv1)
-# import keras
-# # high = Model(inputs=[max1,max2])
-# high = keras.layers.Highway()(conv1)
-# lstm = LSTM()(high)
-#
-# model.add(input_)
-# model.summary()
-from keras.layers.convolutional import Conv2D
-from keras.layers.pooling import MaxPooling2D
-from keras.layers import Dropout
-
-inp = Input(shape=(X.shape[1],))
-print(inp)
-emb = Embedding(len(tk.word_index), 20, input_length=X.shape[1])(inp)
-print(emb)
+# autoencoder params
+timesteps = X.shape[1]
 filters = [20, 10]
 kernels = [2, 3]
-convs = []
+enc_convs = []
+dec_convs = []
+d = 20
+###
+
+enc_inputs = Input(shape=(X.shape[1],))
+encoded = Embedding(len(tk.word_index), d, input_length=X.shape[1])(enc_inputs)
 for i in range(2):
     conv = Conv1D(filters[i],
                   kernels[i],
-                  padding='valid',
+                  padding='same',
                   activation='relu',
-                  strides=1)(emb)
+                  strides=1)(encoded)
     conv = Dropout(0.1)(conv)
     conv = MaxPooling1D()(conv)
-    convs.append(conv)
+    enc_convs.append(conv)
 
-from keras.layers.merge import concatenate
+encoded = concatenate(enc_convs)
+encoded = LSTM(128)(encoded)
 
-out = concatenate(convs)
+decoded = RepeatVector(timesteps)(encoded)
+decoded = LSTM(128, return_sequences=True)(decoded)
 
-conv_model = Model(inputs=inp, outputs=out)
-model.add(conv_model)
-from keras.layers import Highway
-# model.add(Highway())
-model.add(LSTM(128))
-# model.fit(X,y)
-#
-# #
-# # input_array = np.random.randint(1000, size=(32, 10))
-# # model.add(Embedding(1000, 64, input_length=10))
-model.compile('rmsprop', 'mse')
-from keras.utils.vis_utils import plot_model
+for i in range(2):
+    conv = Conv1D(filters[i],
+                  kernels[i],
+                  padding='same',
+                  activation='relu',
+                  strides=1)(decoded)
+    conv = Dropout(0.1)(conv)
+    # conv = MaxPooling1D()(conv)
+    dec_convs.append(conv)
 
-plot_model(model, show_shapes=True)
-model.summary()
+decoded = concatenate(dec_convs)
+decoded = Dense(len(tk.word_index), activation='sigmoid')(decoded)
 
-# output_array = model.predict(X)
-# print(output_array.shape)
-# print(model.predict(X[0]).shape)
+autoencoder = Model(inputs=enc_inputs, outputs=decoded)
+
+# optimizer = RMSprop(lr=0.01)
+# autoencoder.compile(loss='categorical_crossentropy', optimizer=optimizer)
+autoencoder.summary()
+plot_model(autoencoder, to_file="autoencoder.png", show_shapes=True)
+preds = autoencoder.predict(x=X, verbose=2)
+
+
+def sample(preds, temperature=1.0):
+    # helper function to sample an index from a probability array
+    preds = np.asarray(preds).astype('float32')
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
+
+
+inv_map = {v: k for k, v in tk.word_index.iteritems()}
+domains = []
+word = ""
+# print(inv_map)
+for j in range(preds.shape[0]):
+    word = ""
+    for i in range(preds.shape[1]):
+        k = sample(preds[j][i])
+        if k > 0:
+            word = word + inv_map[k]
+    domains.append(word)
+
+print(domains)
+
+from detect_DGA import MyClassifier
+
+rndf = MyClassifier(directory="/home/archeffect/PycharmProjects/detect_DGA/models/RandomForest tra:sup tst:sup")
+true = np.ravel(np.zeros(len(domains), dtype=int))
+res = rndf.predict(domains, true)
